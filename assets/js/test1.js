a36002c27f196b4bda30492be1cb178f7b62a9d3
Undefined-User <leo.ripert@gmail.com>
2022-06-22 14:48:53 +0200

    test1.js replacement line

--- assets/js/test1.js
+++ assets/js/test1.js
@@ -150,6 +150,5 @@ document.getElementById("earned_SQL").value = score1;
 document.getElementById("max_SQL").value = values3.length;
 
 let submit_SQL = document.querySelector('#submit_SQL')
-document.querySelector("#result_SQL").style.display = 'block'
-submit_SQL.addEventListener('click', () => submit_SQL.style.display = 'none')
+submit_SQL.addEventListener('click', () => submit_SQL.style.display = 'none', document.querySelector("#result_SQL").style.display = 'block')
 }
